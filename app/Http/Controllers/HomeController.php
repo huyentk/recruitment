f718b4f5d870b365b6e62da0363599d463a57923
Huyen Tran <huyentk1296@gmail.com>
2017-06-15 01:24:49 +0700

    contact

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,10 @@ class HomeController extends Controller
 
         $companies = Company::all();
         foreach ($companies as $company) {
-            $company->image = file_exists(public_path().'/companies/'.$company->id.'.png') ? Storage::url('/companies/'.$company->id.'.png') : Storage::url('/companies/default.png');
+            $company->image = Storage::url('/companies/'.$company->id.'.png');
+            if($company->image == ''){
+                $company->image = Storage::url('/companies/default.png');
+            }
         }
         return view('homepage')->with([
             'jobs' => $jobs,
